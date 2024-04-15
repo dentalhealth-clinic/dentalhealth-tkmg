ba5e4bd5fd059e6a571ef57b17779440626ae531
Bhavya Kishor <bhavyakishorgatya@gmail.com>
2024-04-15 12:35:00 +0530

    mweb ui fixes

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-44TDFNCP.mjs";import"./chunk-QEPY75FO.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-LU4U7SVE.mjs";import"./chunk-QEPY75FO.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
