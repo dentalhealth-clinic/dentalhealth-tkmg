724be5f6ac8b9bd5931258c4fc5804a732a5ef93
Bhavya Kishor <bhavyakishorgatya@gmail.com>
2024-04-25 19:18:25 +0530

    media query updated

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-QMAXONRT.mjs";import"./chunk-L7B66GZ2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-44HRZNVK.mjs";import"./chunk-L7B66GZ2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
